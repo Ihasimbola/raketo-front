03fbd16faa07643857785603e423e4955f408c1c
Ihasimbola <hasinarasolofndraibe77@gmail.com>
2024-01-04 16:39:47 +0300

    integrating text component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { useRef } from 'react';
 import './App.scss';
 import { Button } from './components/Button/Button';
 import Input from './components/Input/Input';
+import Text from './components/Text/Text';
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Text
+        as="h1"
+        color="light-800"
+        size="2xl"
+      >
+        Title
+      </Text>
       <div className="flex flex-row gap-1">
         <Button size="default" variant="default">Any text</Button>
         <Button size="default" variant="outline">Any text</Button>
@@ -20,6 +25,14 @@ function App() {
         <Button size="default" variant="gray">Any text</Button>
       </div>
       <Input variant="primary" placeholder='Username' ref={inputRef} />
+      <Text
+        color="light-600"
+        size="base"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, commodi! Dolore nulla non officia id voluptatibus doloremque unde enim pariatur nemo labore dolor tempora aperiam fuga, rem deleniti eius expedita.
+      </Text>
+
+      
     </div>
   );
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,24 +1,47 @@
+import { VariantProps, cva } from 'class-variance-authority'
 import React from 'react'
+import { cn } from '../../lib/utils'
 
-type PropsType = {
-  children: React.ReactNode,
-  size: number,
-  variant: string,
-  as: keyof JSX.IntrinsicElements,
-  classname?: string
-}
+const textVariants = cva(
+  "",
+  {
+    variants: {
+      color: {
+        "light-500": "text-gray-500",
+        "light-600": "text-gray-600",
+        "light-700": "text-gray-700",
+        "light-800": "text-gray-800",
+        black: "text-gray-900",
+        white: "text-gray-100"
+      },
+      size: {
+        xs: "text-xs",
+        sm: "text-sm",
+        base: "text-base",
+        lg: "text-lg",
+        xl: "text-xl",
+        "2xl": "text-2xl",
+      }
+    }
+  }
+)
 
-const Text: React.FC<PropsType> = (props) => {
-  const { children, size, variant, classname , as = "p" } = props;
+interface TextProps extends 
+ VariantProps<typeof textVariants> {
+  as?: keyof JSX.IntrinsicElements;
+  children: React.ReactNode;
+  className?: string
+ }
+
+const Text = (props: TextProps) => {
+  const { children, size, className , as = "p", color, ...otherProps } = props;
 
   const Tag = as;
 
   return (
     <Tag
-      style={{
-        fontSize: size,
-        color: variant
-      }}
+     className={cn(textVariants({ color, size }))}
+     {...otherProps}
     >
       { children }
     </Tag>
