4565879a561ff5655f29b180e58fb2b1f7ea7757
Ihasimbola <hasinarasolofndraibe77@gmail.com>
2024-01-04 06:33:58 +0300

    test UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import './App.css';
+import './App.scss';
+import { Button } from './components/Button/Button';
 
 function App() {
   return (
@@ -6,6 +7,14 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <div className="flex flex-row gap-1">
+        <Button size="default" variant="default">Any text</Button>
+        <Button size="default" variant="outline">Any text</Button>
+        <Button size="default" variant="primary">Any text</Button>
+        <Button size="default" variant="secondary">Any text</Button>
+        <Button size="default" variant="danger">Any text</Button>
+        <Button size="default" variant="gray">Any text</Button>
+      </div>
     </div>
   );
 }
